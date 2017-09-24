f0eb400fa4cfded8b11ae84dc9a8058533b9eb81
Alexiuce <alexiuce@qq.com>
2017-09-24 16:49:58 +0800

    fix pic 2x2

--- CocoaDemos/iFeiBo/iFeiBo/PicsView.swift
+++ CocoaDemos/iFeiBo/iFeiBo/PicsView.swift
@@ -19,7 +19,7 @@ class PicsView: NSView {
         didSet{
             if oldValue.count != 0 {subviews.removeAll()}               // 清除旧视图
             let imgCount  = picUrls.count
-
+            var itemInRow  = 3
             switch imgCount {
             case 0:
                 caculateSize = NSZeroSize
@@ -29,6 +29,7 @@ class PicsView: NSView {
                 caculateSize = NSMakeSize(kPicuterWH * 2 + kMargin, kPicuterWH)
             case 4:
                 caculateSize = NSMakeSize(kPicuterWH * 2 + kMargin, kPicuterWH * 2 + kMargin)
+                itemInRow = 2
             default:
                 // 计算行数
                 let row = CGFloat((imgCount - 1) / 3 + 1)
@@ -39,15 +40,20 @@ class PicsView: NSView {
                 
             }
             
+        
             for i in 0..<imgCount {
                 let imageView = NSImageView(frame: NSMakeRect(0, 0, kPicuterWH, kPicuterWH))
                 imageView.imageAlignment = .alignCenter
                 imageView.imageScaling = .scaleNone
-                let pointX = CGFloat(i % 3) * (kPicuterWH + kMargin)
-                let pointY = CGFloat(i / 3) * (kPicuterWH + kMargin)
+                
+                
+                let pointX = CGFloat(i % itemInRow) * (kPicuterWH + kMargin)
+                let pointY = CGFloat(i / itemInRow) * (kPicuterWH + kMargin)
                 imageView.setFrameOrigin(NSMakePoint(pointX, pointY))
                 addSubview(imageView)
-                imageView.kf.setImage(with: ImageResource.init(downloadURL: picUrls[i]))
+               
+                imageView.kf.setImage(with: picUrls[i])
+                
             }
             
         }
